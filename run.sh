42f37d50eb5e3b75560b78871d6fb79ad30dba2e
Martin Angers <martin.n.angers@gmail.com>
2013-07-14 22:38:16 -0400

    fix run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-trofaf -p 80 -n "Ø value" -t "a wysiwyg hypertext cybersite" -r 5 -b "http://0value.com"
+# sudo required for port 80
+sudo trofaf -p 80 -n "Ø value" -t "a wysiwyg hypertext cybersite" -r 5 -b "http://0value.com"
